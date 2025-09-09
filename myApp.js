51efc8e4abe90d1e8d3bf1803de9e3898d56e357
Dilorom <reta4me@gmail.com>
2025-09-09 10:19:39 -0700

    Update README.md

--- myApp.js
+++ myApp.js
@@ -1,16 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`My CyberSec App started on port ${PORT}`);
+});
 
 
 
