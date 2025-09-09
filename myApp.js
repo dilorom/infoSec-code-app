0213aa2af61a1ceb94d4ec4f39d5da880d25cbb9
Dilorom <reta4me@gmail.com>
2025-09-09 12:23:34 -0700

    HelmetHide removed

--- myApp.js
+++ myApp.js
@@ -2,21 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-  console.log(`My CyberSec App started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy());
 
 
 
@@ -52,5 +38,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`My CyberSercApp started on port ${port}`);
 });
